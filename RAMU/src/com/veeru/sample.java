e77b152b2efecc2c76714d13c04cd588b125b6da
Veeraiah Madhira <vmadhira@blackberry.com>
2014-06-19 16:50:02 +0530

    commit-4

--- RAMU/src/com/veeru/sample.java
+++ RAMU/src/com/veeru/sample.java
@@ -1,12 +1,5 @@
 package com.veeru;
 
 public class sample {
-	public static void main(String[] args) {
-		System.out.println("AAAAA");
-		System.out.println("AAAAA");
-		System.out.println("AAAAA");
-		System.out.println("AAAAA");
-		System.out.println("AAAAA");
-	}
 
 }
